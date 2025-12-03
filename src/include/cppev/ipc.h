590e6a93b678fe3f6a6ce375871df99930ec10fb
ayh <aoyuhang0707@qq.com>
2025-12-03 14:35:27 +0800

    IPC模块已完善

--- src/include/cppev/ipc.h
+++ src/include/cppev/ipc.h
@@ -16,7 +16,7 @@ class CPPEV_PUBLIC shared_memory final
 {
 public:
     shared_memory(const std::string &name, int size, mode_t mode = 0600);
-
+    //系统资源类的封装通常都禁止拷贝。
     shared_memory(const shared_memory &) = delete;
     shared_memory &operator=(const shared_memory &) = delete;
     shared_memory(shared_memory &&other) noexcept;
@@ -28,6 +28,8 @@ public:
     SharedClass *construct(Args &&...args)
     {
         SharedClass *object =
+            // 不要分配内存！ 我手里已经有一块地了（就是 ptr_，指向那块共享内存）。
+            // 请你直接在这块地上盖房子（调用构造函数）。
             new (ptr_) SharedClass(std::forward<Args>(args)...);
         if (object == nullptr)
         {
@@ -59,21 +61,23 @@ private:
 class CPPEV_PUBLIC semaphore final
 {
 public:
+    // 创建和初始化信号量的地方
     explicit semaphore(const std::string &name, mode_t mode = 0600);
 
+    // 系统资源（如文件描述符或系统信号量句柄）是独占的，不适合进行浅拷贝或自动深拷贝。
     semaphore(const semaphore &) = delete;
     semaphore &operator=(const semaphore &) = delete;
     semaphore(semaphore &&other) noexcept;
     semaphore &operator=(semaphore &&other) noexcept;
 
     ~semaphore() noexcept;
-
+    // 非阻塞
     bool try_acquire();
-
+    // 阻塞
     void acquire(int count = 1);
 
     void release(int count = 1);
-
+    // 销毁或删除系统中具名信号量
     void unlink();
 
     bool creator() const noexcept;
@@ -81,10 +85,11 @@ public:
 private:
     void move(semaphore &&other) noexcept;
 
+    // 信号量的名称，用于在系统中查找或创建信号量
     std::string name_;
-
+    // 这是 POSIX 信号量 API（如 sem_open）返回的句柄
     sem_t *sem_;
-
+    // 记录当前对象是否是信号量的创建者
     bool creator_;
 };
 
--- src/lib/ipc.cpp
+++ src/lib/ipc.cpp
@@ -15,11 +15,15 @@ shared_memory::shared_memory(const std::string &name, int size, mode_t mode)
     int fd = shm_open(name_.c_str(), O_RDWR, mode);
     if (fd < 0)
     {
+        // ENOENT（对象不存在）
         if (errno == ENOENT)
         {
+            // O_EXCL 保证了操作的原子性：如果创建成功，当前进程就是创建者（creator_ = true）。
+            // mode指定了新创建的共享内存对象的访问权限
             fd = shm_open(name_.c_str(), O_RDWR | O_CREAT | O_EXCL, mode);
             if (fd < 0)
             {
+                // 被其他进程抢先创建
                 if (errno == EEXIST)
                 {
                     fd = shm_open(name_.c_str(), O_RDWR, mode);
@@ -46,6 +50,8 @@ shared_memory::shared_memory(const std::string &name, int size, mode_t mode)
 
     if (creator_)
     {
+        // 使用 ftruncate 函数来设置其大小（size_）
+        //只有在设置了正确的大小后，后续的 mmap 调用才能成功地将这块内存区域映射到进程的地址空间。
         int ret = ftruncate(fd, size_);
         if (ret == -1)
         {
@@ -83,6 +89,10 @@ shared_memory &shared_memory::operator=(shared_memory &&other) noexcept
     return *this;
 }
 
+// 个完整的共享内存清理流程通常是：先在析构函数中调用 munmap（解除映射）
+// 然后在创建者进程的生命周期结束时调用 shm_unlink个完整的共享内存清理流程通常是：
+// 先在析构函数中调用 munmap（解除映射），然后在创建者进程的生命周期结束时调用 shm_unlink
+// 在对象生命周期结束时，解除内存映射，从而释放当前进程对共享内存区域的访问权。
 shared_memory::~shared_memory() noexcept
 {
     if (ptr_ != nullptr && size_ != 0)
@@ -91,6 +101,7 @@ shared_memory::~shared_memory() noexcept
     }
 }
 
+//  解除映射
 void shared_memory::unlink()
 {
     if (name_.size() && (shm_unlink(name_.c_str()) == -1))
@@ -138,6 +149,7 @@ semaphore::semaphore(const std::string &name, mode_t mode)
             sem_ = sem_open(name_.c_str(), O_CREAT | O_EXCL, mode, 0);
             if (sem_ == SEM_FAILED)
             {
+                // 解决竞态问题，被其他进程抢先创建
                 if (errno == EEXIST)
                 {
                     sem_ = sem_open(name_.c_str(), 0);
@@ -194,6 +206,8 @@ bool semaphore::try_acquire()
 {
     if (sem_trywait(sem_) == -1)
     {
+        // sem_trywait() 返回失败的最常见原因，表示信号量计数当前为 0。由于 try_acquire 是非阻塞的，它不能等待。
+        // sem_trywait() 在完成前被一个信号中断。由于 EAGAIN 是非阻塞的，通常也将其视为未能成功获取资源
         if (errno == EINTR || errno == EAGAIN)
         {
             return false;
@@ -210,6 +224,7 @@ void semaphore::acquire(int count)
 {
     for (int i = 0; i < count; ++i)
     {
+        // 阻塞等待三次
         if (sem_wait(sem_) == -1)
         {
             throw_system_error("sem_wait error");
